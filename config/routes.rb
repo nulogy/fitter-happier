46aa8fcc7606060887c144535cd5e29bf61fe2f9
Kevin Chen <kevinc@nology.com>
2015-10-19 14:08:33 -0400

    Modify routes to support Rails 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  match '/fitter_happier' => 'fitter_happier#index'
-  match '/fitter_happier/site_check' => 'fitter_happier#site_check'
-  match '/fitter_happier/site_and_database_check' => 'fitter_happier#site_and_database_check'
+  get '/fitter_happier' => 'fitter_happier#index'
+  get '/fitter_happier/site_check' => 'fitter_happier#site_check'
+  get '/fitter_happier/site_and_database_check' => 'fitter_happier#site_and_database_check'
 end
