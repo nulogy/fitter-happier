f369a6e802620b66284d08e1414c1dc271b462bb
Evan Brodie <evanb@nulogy.com>
2016-09-27 14:30:41 -0400

    replaces deprecated render :text with render :plain

--- app/controllers/fitter_happier/fitter_happier_controller.rb
+++ app/controllers/fitter_happier/fitter_happier_controller.rb
@@ -3,18 +3,18 @@ module FitterHappier
     layout nil
 
     def index
-      render(:text => "FitterHappier Site Check Passed\n")
+      render(:plain => "FitterHappier Site Check Passed\n")
     end
 
     def site_check
       time = Time.now.to_formatted_s(:rfc822)
-      render(:text => "FitterHappier Site Check Passed @ #{time}\n")
+      render(:plain => "FitterHappier Site Check Passed @ #{time}\n")
     end
 
     def site_and_database_check
       version = DatabaseCheck.schema_version
       time = Time.now.to_formatted_s(:rfc822)
-      render(:text => "FitterHappier Site and Database Check Passed @ #{time}\nSchema Version: #{version}\n")
+      render(:plain => "FitterHappier Site and Database Check Passed @ #{time}\nSchema Version: #{version}\n")
     end
 
     private
